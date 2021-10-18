9e04abfb611c287c3df9cd90d4d9fc1e6ee4475b
Turi Csaba <csabaek@gmail.com>
2021-10-18 06:14:18 +0200

    set cors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,25 @@
-
-module.exports = ({
+module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/pages/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   experimental: {
     scrollRestoration: true,
   },
@@ -14,8 +34,8 @@ module.exports = ({
     return config;
   },
   images: {
-    domains: ['cdn.sanity.io'],
+    domains: ["cdn.sanity.io"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-});
+};
